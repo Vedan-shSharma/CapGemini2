abe3780333b086b1f81860a7d1002805805c4452
Vedan-shSharma <107530627+Vedan-shSharma@users.noreply.github.com>
2025-04-26 15:25:19 +0530

    Delete Jenkins_Pipeline_Setup directory

--- Jenkins_Pipeline_Setup/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3001;
-
-app.get('/', (req, res) => res.send('Hello from Dockerized Node.js App by Vedansh Sharma!'));
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
